62574b289e48f0b85a194a23fe855b835fedf867
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2021-05-20 08:32:12 +0100

    BL-12177 Initial setup (#1)
    * Composer init
    
    * Add Code of Conduct
    
    and Contributing guide and Readme
    
    * Setup PHPUnit and run example unit test
    
    * Add src placeholder
    
    * Add PR template

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Authentication\Cognito\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testInit()
+    {
+        $this->assertTrue(true);
+    }
+}
